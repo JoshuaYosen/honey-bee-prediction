0cf4fa7d9444e95b6718d5ef707b891e284cb573
Joshua Yosen <lereyendoformaggio@gmail.com>
2019-02-22 09:15:56 -0500

    Add predict file

---
+++ predict.py
@@ -0,0 +1,41 @@
+import seaborn
+import pandas as pd
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import linear_model
+
+df = pd.read_csv("https://s3.amazonaws.com/codecademy-content/programs/data-science-path/linear_regression/honeyproduction.csv")
+
+print(df.head())
+
+prod_per_year = df.groupby('year').totalprod.mean().reset_index()
+
+X = prod_per_year.year
+
+X = X.values.reshape(-1, 1)
+
+y = prod_per_year.totalprod
+
+plt.scatter(X, y)
+
+
+regr = linear_model.LinearRegression()
+regr.fit(X, y)
+print(regr.coef_[0])
+print(regr.intercept_)
+
+m = -88303.18915238192
+b = 181208083.10732976
+
+y_predict = regr.predict(X)
+
+plt.plot(X, y_predict)
+
+X_future = np.array(range(2013, 2050))
+
+X_future = X_future.reshape(-1, 1)
+
+future_predict = regr.predict(X_future)
+
+plt.plot(X_future, future_predict)
+plt.show()
